acedb25f4a3661456ae85b7ba212e1acac5cb296
KKnFinite <kknfinite@gmail.com>
2026-09-25 12:11:35 -0500

    Keep cache generator on v60

--- tools/build_assets.py
+++ tools/build_assets.py
@@ -363,7 +363,7 @@ def update_home():
 def update_service_worker():
     path = ROOT / "static" / "service-worker.js"
 
-    content = """const CACHE_NAME = 'wpm-shell-v59';
+    content = """const CACHE_NAME = 'wpm-shell-v60';
 
 const APP_SHELL = [
   '/',
