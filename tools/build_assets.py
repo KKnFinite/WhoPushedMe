30531354fc007376c7d688d45023ff337cbdd57b
KKnFinite <kknfinite@gmail.com>
2026-09-25 03:38:28 -0500

    Keep cache generator on v52

--- tools/build_assets.py
+++ tools/build_assets.py
@@ -360,7 +360,7 @@ def update_home():
 def update_service_worker():
     path = ROOT / "static" / "service-worker.js"
 
-    content = """const CACHE_NAME = 'wpm-shell-v51';
+    content = """const CACHE_NAME = 'wpm-shell-v52';
 
 const APP_SHELL = [
   '/',
