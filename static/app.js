11cc1276002ceb4b8408b0343915e92d2f2b4af5
KKnFinite <kknfinite@gmail.com>
2026-09-25 17:02:34 -0500

    Plant step two mini on button

--- static/app.js
+++ static/app.js
@@ -1190,7 +1190,7 @@
       const transparentBottom =
         setupMiniOpaqueBottomRatio(mini) * miniRect.height;
       const visibleBottom = miniRect.bottom - transparentBottom;
-      const shift = Math.round(buttonRect.top - visibleBottom + 8);
+      const shift = Math.round(buttonRect.top - visibleBottom);
       stage.style.setProperty('--setup-mini-y', `${shift}px`);
     });
   };
